ccb5a91fb59e741a90d7e5313fb6522f0b56f666
Lorna Sherwood <lornasherwood@gmail.com>
2016-11-28 16:54:59 +0000

    Completed Part B

--- specs/student_specs.rb
+++ specs/student_specs.rb
@@ -3,9 +3,6 @@ require_relative('../student.rb')
 
 class StudentSpec < MiniTest::Test
 
-
-
-
   def test_can_create_student
     student = Student.new("Kristin", 9, "Ruby")
   end
@@ -43,3 +40,59 @@ class StudentSpec < MiniTest::Test
   end
 
 end
+
+
+class TeamSpec < MiniTest::Test
+
+  def test_can_create_team
+    team = Team.new("Huskies", ["Spot"], "Jim", 10)
+  end
+
+  def test_can_get_team
+    team = Team.new("Huskies", ["Spot"], "Jim", 10)
+    assert_equal("Huskies", team.get_team_name)
+  end
+
+  def test_can_get_member_names
+    team = Team.new("Collies", ["Pip"], "Shepherd", 20)
+    assert_equal(["Pip"], team.get_member_names)
+  end
+
+  def test_can_get_coach_name
+    team = Team.new("Collies", ["Pip"], "Shepherd", 20)
+    assert_equal("Shepherd", team.get_coach_name)
+  end
+
+  def test_can_update_coach_name
+    team = Team.new("Huskies", ["Spot"], "Jim", 10)
+    team.update_coach_name("Musher")
+    assert_equal("Musher", team.get_coach_name)
+  end
+
+  def test_add_member_names
+    team = Team.new("Collies", ["Pip"], "Shepherd", 20)
+    team.add_member_names("Fly")
+    assert_equal(["Pip", "Fly"], team.get_member_names)
+  end
+
+  def test_member_is_present
+    team = Team.new("Collies", ["Pip", "Fly"], "Shepherd", 20)
+    assert_equal(true, team.member_is_present("Fly"))
+  end
+
+  def test_can_get_points
+    team = Team.new("Collies", ["Pip", "Fly"], "Shepherd", 20)
+    assert_equal(20, team.get_points)
+  end
+
+  def test_win_or_lose
+    team = Team.new("Collies", ["Pip", "Fly"], "Shepherd", 20)
+    team.win_or_lose("win",10)
+    assert_equal(30, team.get_points)
+  end
+
+
+
+end
+
+
--- student.rb
+++ student.rb
@@ -1,40 +1,80 @@
 class Student
 
-def initialize (name, cohort, fav_language)
-  @name = name
-  @cohort = cohort
-  @fav_language = fav_language
+  def initialize (name, cohort, fav_language)
+    @name = name
+    @cohort = cohort
+    @fav_language = fav_language
+  end
+
+  def get_name
+  return @name
+  end
+
+  def update_name(name)
+    @name = name
+  end
+
+  def get_cohort
+    return @cohort
+  end
+
+  def update_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def student_talk
+    return "I can talk!"
+  end
+
+  def favourite_language
+    return "I love #{@fav_language}!"
+  end
+
 end
 
-def get_name
-return @name
-end
+class Team
 
-def update_name(name)
-  @name = name
-end
+  def initialize(team_name, member_names, coach_name, points )
+    @team_name = team_name
+    @member_names = member_names
+    @coach_name = coach_name
+    @points = points
+  end
 
-def get_cohort
-  return @cohort
-end
+  @points = 0
 
-def update_cohort(cohort)
-  @cohort = cohort
-end
+  def get_team_name
+    return @team_name
+  end
 
-def student_talk
-  return "I can talk!"
-end
+  def get_member_names
+    return @member_names
+  end
 
-def favourite_language
-  return "I love #{@fav_language}!"
-end
+  def get_coach_name
+    return @coach_name
+  end
 
+  def update_coach_name(coach_name)
+    @coach_name = coach_name
+  end
 
+  def add_member_names(member_name)
+    @member_names << member_name
+  end
 
+  def member_is_present(member_name)
+    if @member_names.include? member_name
+      return true
+    end
+  end
 
+  def get_points
+    return @points
+  end
 
-
-
+  def win_or_lose(outcome, points)
+    @points += points if outcome = "win"
+  end
 
 end
